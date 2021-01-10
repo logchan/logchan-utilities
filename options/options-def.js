d148a5bc614dffeb61552aabfd3a6ab9735509c6
logchan <logchan@outlook.com>
2021-01-09 20:23:36 -0500

    update options

--- options/options-def.js
+++ options/options-def.js
@@ -7,21 +7,28 @@ logchan_util_options_def = new options_group("Special functions", "Very logchan-
     new button_option_entry("Delete some history", () => {
         let targets = ["//www.pixiv.net", "//t.cn"]
         let deleter = (results) => {
+            addLog(`Found ${results.length} entries in history`)
+            let count = 0
             results.forEach(r => {
                 if (targets.every(t => r.url.indexOf(t) < 0)) {
                     return
                 }
-                chrome.history.deleteUrl({ url: r.url }, () => 
-                    addLog(`Delete: ${r.title} - ${r.url}`)
-                )
+                chrome.history.deleteUrl({ url: r.url })
+                count += 1
             })
+            addLog(`Deleted ${count} entries`)
         }
 
         chrome.history.search( { 
             text: "", 
-            maxResults: 65536,
+            maxResults: 131072,
             startTime: 0
-        }, deleter )
+        }, deleter)
+    }),
+    new button_option_entry("Clear downloads", () => {
+        chrome.downloads.erase({}, results => {
+            addLog(`Erased ${results.length} downloads`)
+        })
     })
 ])
 
